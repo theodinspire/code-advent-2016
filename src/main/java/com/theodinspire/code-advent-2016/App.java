9d44ebd924f854362c684f5b4e89b572ffdd6dcf
theodinspire <EdouardManet1>
2016-12-13 12:12:25 -0600

    Building maven project

---
+++ src/main/java/com/theodinspire/code-advent-2016/App.java
@@ -0,0 +1,13 @@
+package com.theodinspire.code-advent-2016;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
