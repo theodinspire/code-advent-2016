7b2bdee155f1ddf9df34f2b067348b35f7558ecd
theodinspire <EdouardManet1>
2016-12-14 16:24:44 -0600

    Day two solved, now with predicates

--- src/main/java/com/theodinspire/codeadvent/BathroomKeyPad.java
+++ src/main/java/com/theodinspire/codeadvent/BathroomKeyPad.java
@@ -2,31 +2,51 @@ package com.theodinspire.codeadvent;
 
 import java.io.BufferedReader;
 import java.io.FileReader;
+import java.util.function.Predicate;
 
 /**
  * Created by ecormack on 12/14/2016.
  */
 public class BathroomKeyPad {
-    private static String[][] PAD = {{ "1", "2", "3"}, {"4", "5", "6"}, {"7", "8", "9"}};
-    private static int MIN_INDEX = 0;
-    private static int MAX_INDEX = 2;
-    private static int DEFAULT_INDEX = 1;
+    private static String[][] DEFAULT_PAD = {{ "1", "2", "3"}, {"4", "5", "6"}, {"7", "8", "9"}};
 
-    private int horzIndex;
-    private int vertIndex;
+    private String[][] pad = DEFAULT_PAD;
+    private int horzIndex = 1;
+    private int vertIndex = 1;
+
+    private Predicate<int[]> moveCondition = p -> {
+        return Math.abs(p[0] - 1) < 2 && Math.abs(p[1] - 1) < 2;
+    };
+
+    public void setPad(String[][] pad) {
+        this.pad = pad;
+    }
+
+    public void setMoveCondition(Predicate<int[]> moveCondition) {
+        this.moveCondition = moveCondition;
+    }
+
+    public void setHorzIndex(int horzIndex) { this.horzIndex = horzIndex; }
+    public void setVertIndex(int vertIndex) { this.vertIndex = vertIndex; }
+
+    public void setFinger(int x, int y) { setHorzIndex(x); setVertIndex(y); }
+    public void setFinger(int[] a)      { setFinger(a[0], a[1]); }
 
     public BathroomKeyPad(int horzIndex, int vertIndex) {
         this.horzIndex = horzIndex;
         this.vertIndex = vertIndex;
     }
-    public BathroomKeyPad() { this(DEFAULT_INDEX, DEFAULT_INDEX); }
+    public BathroomKeyPad() {}
 
-    private void up()       { vertIndex = Math.max(vertIndex - 1, MIN_INDEX); }
-    private void down()     { vertIndex = Math.min(vertIndex + 1, MAX_INDEX); }
-    private void left()     { horzIndex = Math.max(horzIndex - 1, MIN_INDEX); }
-    private void right()    { horzIndex = Math.min(horzIndex + 1, MAX_INDEX); }
+    private void up()       { tryMove(horzIndex, vertIndex - 1); }
+    private void down()     { tryMove(horzIndex, vertIndex + 1); }
+    private void left()     { tryMove(horzIndex - 1, vertIndex); }
+    private void right()    { tryMove(horzIndex + 1, vertIndex); }
 
-    public void reset()     { horzIndex = DEFAULT_INDEX; vertIndex = DEFAULT_INDEX; }
+    private void tryMove(int x, int y) {
+        int[] tst = {x, y};
+        if (moveCondition.test(tst)) { horzIndex = x; vertIndex = y; }
+    }
 
     public String getDigit(String instructions) {
         for (char instruction : instructions.toUpperCase().toCharArray()) {
@@ -38,7 +58,7 @@ public class BathroomKeyPad {
             }
         }
 
-        return PAD[vertIndex][horzIndex];
+        return pad[vertIndex][horzIndex];
     }
 
     public String getCode(String filepath) {
@@ -60,6 +80,31 @@ public class BathroomKeyPad {
 
     public static void main(String[] args) {
         BathroomKeyPad pad = new BathroomKeyPad();
-        System.out.println("Key code is: " + pad.getCode("src/resources/BathroomKeyPad/instructions.txt"));
+        System.out.println("9-digit, square pad:");
+        System.out.println("Test key code is: " + pad.getCode("src/resources/BathroomKeyPad/test.txt"));
+        pad.setFinger(1, 1);
+        System.out.println("Actual key code is: " +
+                pad.getCode("src/resources/BathroomKeyPad/instructions.txt"));
+
+        System.out.println();
+
+        BathroomKeyPad odd = new BathroomKeyPad(0, 2);
+        String[][] padKeys = {{    "",  "", "1",  "",  ""},
+                                {  "", "2", "3", "4",  ""},
+                                { "5", "6", "7", "8", "9"},
+                                {  "", "A", "B", "C",  ""},
+                                {  "",  "", "D",  "",  ""}};
+        odd.setPad(padKeys);
+        odd.setMoveCondition(p -> {
+            int x = Math.abs(p[0] - 2);
+            int y = Math.abs(p[1] - 2);
+            return x + y < 3;
+        });
+
+        System.out.println("13-digit, diamond pad:");
+        System.out.println("Test key code is: " + odd.getCode("src/resources/BathroomKeyPad/test.txt"));
+        odd.setFinger(0, 2);
+        System.out.println("Actual key code is: " +
+                odd.getCode("src/resources/BathroomKeyPad/instructions.txt"));
     }
 }
