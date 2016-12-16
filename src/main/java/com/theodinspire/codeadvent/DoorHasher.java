e75e58a1c1637d9189e20afcab9b1df02d2ea192
theodinspire <EdouardManet1>
2016-12-16 12:07:53 -0600

    Day 5 complete

---
+++ src/main/java/com/theodinspire/codeadvent/DoorHasher.java
@@ -0,0 +1,94 @@
+package com.theodinspire.codeadvent;
+
+import java.security.*;
+
+/**
+ * Created by ecormack on 12/16/2016.
+ */
+public class DoorHasher {
+    public static void main(String[] args) {
+        String doorID = "ugkcyxxp";
+
+        //System.out.println("First door: " + firstDoor(doorID));
+
+        System.out.println(secondDoor(doorID));
+    }
+
+    private static String firstDoor(String input) {
+        MessageDigest md;
+        String algorithm = "MD5";
+        StringBuilder builder = new StringBuilder();
+        int count = 0;
+        int index = 0;
+
+        try {
+            md = MessageDigest.getInstance(algorithm);
+
+            while (count < 8) {
+                byte[] hash = md.digest((input + index++).getBytes());
+                if (hash[0] == 0 && hash[1] == 0 && (hash[2] & 0xf0) == 0) {
+                    builder.append(String.format("%x", hash[2]));
+                    ++count;
+                }
+            }
+        } catch (NoSuchAlgorithmException e) {
+            System.out.println("Algorithm " + algorithm + " does not exist");
+            e.printStackTrace();
+            System.exit(1);
+        }
+
+        return builder.toString();
+    }
+
+    private static String secondDoor(String input) {
+        PassBuilder passBuilder = new PassBuilder();
+
+        MessageDigest md;
+        String algorithm = "MD5";
+        int index = 0;
+
+        try {
+            md = MessageDigest.getInstance(algorithm);
+
+            while (!passBuilder.isComplete()) {
+                byte[] hash = md.digest((input + index++).getBytes());
+                if (hash[0] == 0 && hash[1] == 0 && (hash[2] & 0xf0) == 0) {
+                    passBuilder.addCharacter(hash[2], hash[3]);
+                }
+            }
+        } catch (NoSuchAlgorithmException e) {
+            System.out.println("Algorithm " + algorithm + " does not exist");
+            e.printStackTrace();
+            System.exit(1);
+        }
+
+        return passBuilder.getPassword();
+    }
+
+    private static class PassBuilder {
+        private boolean complete = false;
+
+        private String password = "ABCDEFGH";
+
+        public boolean isComplete() {
+            return complete;
+        }
+
+        public String getPassword() {
+            return password;
+        }
+
+        public void addCharacter(byte index, byte replacement) {
+            char nova = String.format("%02x", replacement).charAt(0);
+            char olda = (char) (index + 65);
+
+            password = password.replace(olda, nova);
+
+            System.out.println(password.replaceAll("[A-H]", "-"));
+
+            if (!password.matches("\\w*[A-H]\\w*")) {
+                complete = true;
+            }
+        }
+    }
+}
--- src/main/java/com/theodinspire/codeadvent/RoomKeys/RoomKeyChecker.java
+++ src/main/java/com/theodinspire/codeadvent/RoomKeys/RoomKeyChecker.java
@@ -2,6 +2,7 @@ package com.theodinspire.codeadvent.RoomKeys;
 
 import java.io.BufferedReader;
 import java.io.FileReader;
+import java.util.Stack;
 
 /**
  * Created by ecormack on 12/15/2016.
@@ -9,6 +10,8 @@ import java.io.FileReader;
 public class RoomKeyChecker {
     public static void main(String[] args) {
         int sum = 0;
+        Stack<RoomKey> keys = new Stack<>();
+
         try (BufferedReader reader =
                      new BufferedReader(new FileReader("src/resources/RoomKeyChecker/input.txt"))) {
             String line;
@@ -17,9 +20,10 @@ public class RoomKeyChecker {
 
             while ((line = reader.readLine()) != null) {
                 RoomKey key = new RoomKey(line);
-                boolean isTrue = checker.validate(key);
-                //System.out.println(isTrue);
-                sum += (isTrue ?key.getSector() : 0);
+                if (checker.validate(key)) {
+                    sum += key.getSector();
+                    keys.push(key);
+                }
             }
         } catch (Exception e) {
             System.out.println("Something went wrong in the RoomKeyChecker");
@@ -27,5 +31,37 @@ public class RoomKeyChecker {
             System.exit(1);
         }
         System.out.println(sum);
+        System.out.println();
+
+        int count = 0;
+
+        for (RoomKey key : keys) {
+            ++count;
+            //System.out.println(decypherKey(key));
+            String name = decypherKey(key);
+            if (name.contains("north") || name.contains("pole")) {
+                System.out.println(String.format("%4d. %s", ++count, name));
+                if (count % 10 == 0)
+                    try {System.in.read();} catch (Exception e) {System.out.println("Continuing");}
+            }
+        }
+    }
+
+    public static String decypherKey(RoomKey key) {
+        StringBuilder cracked = new StringBuilder();
+        int sector = key.getSector();
+        int aShift = (int) 'a';
+
+        for (char c : key.getKey().toCharArray()) {
+            if (c == '-')
+                cracked.append(" ");
+            else if (c >= 'a' && c <= 'z') {
+                cracked.append((char) (((((int) c) + (sector - aShift)) % 26) + aShift));
+            }
+        }
+
+        cracked.append(": ").append(sector);
+
+        return cracked.toString();
     }
 }
